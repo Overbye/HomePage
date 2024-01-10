d45c2416730668f9881a8bcb859102751705ee59
Overbye <tengranlingruo@sina.com>
2024-01-10 09:34:16 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, Wordpress, ClipboardList, Robot, BookReader, Gift, Location, ArrowAlipay, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, Wordpress, ClipboardList, Robot, BookReader, Gift, Location, ArrowAlipay, PaperPlane, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -76,6 +76,7 @@ const siteIcon = {
   Gift,
   Location,
   Alipay,
+  PaperPlane,
   LaptopCode,
 };
 
